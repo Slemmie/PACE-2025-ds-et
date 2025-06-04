03f5a2314c4775c89339817dab31b9c129cbf648
Slemmie <thorvejeneriksen@gmail.com>
2025-06-04 11:47:18 +0200

    fetch.sh also creates a testset ordered like in the leaderboard

--- testset/fetch.sh
+++ testset/fetch.sh
@@ -18,16 +18,22 @@ cp "ds_verifier/Dominating Set Verifier/src/test/resources/testset/"*.sol ./off_
 rm -rf ./ds_verifier
 
 # prelimenary #100
-rm -rf ./off_100
-mkdir ./off_100
 if [ -d "PACE2025-instances" ]; then
 	echo "cannot complete intermediate step: 'PACE2025-instances' directory already exists"
 	exit 1
 fi
+rm -rf ./off_100 ./off_100_lbo
+mkdir ./off_100 ./off_100_lbo
 git clone --depth 1 --filter=blob:none --sparse https://github.com/MarioGrobler/PACE2025-instances.git
 cd PACE2025-instances
 git sparse-checkout set "ds/exact"
+cp ds/exact/*.gr ../off_100/
+i=1
+tail -n +2 3223_ds_exact.csv | head -n 100 | while IFS=, read -r _id name; do
+	name="${name//$'\r'/}"
+	printf -v idx "%03d" "$i"
+	cp "ds/exact/$name" "../off_100_lbo/${idx}_${name}"
+	i=$((i+1))
+done
 cd ..
-cp "PACE2025-instances/ds/exact/"*.gr ./off_100/
-cp "PACE2025-instances/ds/exact/"*.sol ./off_100/
-rm -rf ./PACE2025-instances
+rm -rf PACE2025-instances
